5368247e34fb3886f82cb9b2d425c5188c9616a7
Anastasia <silutina.n@yandex.ru>
2022-10-23 23:44:42 +0300

    task7.5

---
+++ 7.5_summaDiagonaly/Program.cs
@@ -0,0 +1,68 @@
+﻿// 7.5 В матрице чисел найти сумму элементов главной диагонали
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int FindSumMainDiagonal(int[,] matr)
+{
+    int sum = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)     // откорректировено после семинара: убран второй цикл
+    {
+        sum = sum + matr[i, i];  
+    }
+    return sum;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine("Матрица:");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] > 100) System.Console.Write($"{matr[i, j]}   ");
+            else if (matr[i, j] > 10) System.Console.Write($" {matr[i, j]}   ");
+            else if (matr[i, j] > 0) System.Console.Write($"  {matr[i, j]}   ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void PrintMainDiagonal(int[,] matrix)
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine("Главная диагональ:");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) System.Console.Write(matrix[i, j]);
+            else System.Console.Write("      ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = 3;
+int col = 6;
+int min = 1;
+int max = 1000;
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+PrintMainDiagonal(matrix);
+int sumDiagonal = FindSumMainDiagonal(matrix);
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма элементов главной диагонали матрицы составляет: {sumDiagonal}");
+System.Console.WriteLine();
